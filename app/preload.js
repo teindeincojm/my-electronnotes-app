22fa0e38bcfd25b349ad90b027ea3c80490b7bf2
teindeincojm <teindeincojm@gmail.com>
2022-10-31 04:14:46 -0400

    Initial file structure project my-electronnotes-app

---
+++ app/preload.js
@@ -0,0 +1,23 @@
+// preload.js
+
+const { contextBridge } = require('electron')
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  // we can also expose variables, not just functions
+})
+
+// All the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
